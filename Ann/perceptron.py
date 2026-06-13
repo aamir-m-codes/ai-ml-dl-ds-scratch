91f1801207d1b67f32a05fde76fe40f418bf71e8
aamir-m-codes <aamirmaqsood.codes@gmail.com>
2026-06-13 12:43:26 +0500

    documented with comments

--- Ann/perceptron.py
+++ Ann/perceptron.py
@@ -2,17 +2,22 @@ import numpy as np
 import random
 import math
 
+
 def perceptron():
+    # generate inputs
     X = np.array([random.random() for _ in range(3)])
+    # generate weights
     W = np.array([random.random() for _ in range(3)])
+    # generate bias
     b = random.random()
+    # matrix multiplication (for multiply every input with its corresponding weight)
     y = np.dot(X, W.T)
+    # add bias
     y += b
-    
+
     # apply activation function on y -> sigmoid formula: 1/(1 + e^-x)
     output = 1 / (1 + math.exp(-y))
     print(f"Final Output: {output}")
-    
 
 
 """
